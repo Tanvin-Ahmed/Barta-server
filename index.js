ca85daa1c6c85d4d51001f0e465144d64fe3c9af
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2021-11-28 07:45:47 -0800

    fixed img upload bug and some other bug

--- index.js
+++ index.js
@@ -5,24 +5,27 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import userAccount, {
-  updateChatList,
-  userIsOffLine,
-  userIsOnline,
+	updateChatList,
+	userIsOffLine,
+	userIsOnline,
 } from "./route/userAccount.js";
 import message, { oneOneMessageFromSocket } from "./route/message.js";
 import groupAccount, { updateGroupInformation } from "./route/groupAccount.js";
 import groupMessage, { groupChatFromSocket } from "./route/groupMessage.js";
 import generateNewToken from "./route/generateNewToken.js";
+import helmet from "helmet";
+// import fileUpload from "express-fileupload";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(helmet()); // for security purposes
 dotenv.config();
 // app.use(fileUpload());
 
 const httpServer = createServer(app);
 export const io = new Server(httpServer, {
-  cors: true,
+	cors: true,
 });
 
 // data base
@@ -33,178 +36,178 @@ const socketToRoom = {};
 let user = {};
 
 mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => {
-    console.log("database connected");
-    io.on("connection", (socket) => {
-      socket.on("user-info", (userInfo) => {
-        user[socket.id] = userInfo.email;
-        updateChatList(socket);
-        updateGroupInformation(socket, userInfo.email);
-        userIsOnline(userInfo.email);
-        socket.broadcast.emit("user-status", { ...userInfo, status: "active" });
-      });
+	.connect(uri, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log("database connected");
+		io.on("connection", socket => {
+			socket.on("user-info", userInfo => {
+				user[socket.id] = userInfo.email;
+				updateChatList(socket);
+				updateGroupInformation(socket, userInfo.email);
+				userIsOnline(userInfo.email);
+				socket.broadcast.emit("user-status", { ...userInfo, status: "active" });
+			});
 
-      socket.on("join", ({ roomId }) => {
-        groupChatFromSocket(socket, roomId);
-        oneOneMessageFromSocket(socket, roomId);
-      });
+			socket.on("join", ({ roomId }) => {
+				groupChatFromSocket(socket, roomId);
+				oneOneMessageFromSocket(socket, roomId);
+			});
 
-      // private call
-      socket.on("is-receiver-free", (data) => {
-        socket.emit("is-me-free", data);
-      });
+			// private call
+			socket.on("is-receiver-free", data => {
+				socket.emit("is-me-free", data);
+			});
 
-      socket.on("my-status-for-call", (data) => {
-        socket.emit("receiver-status-for-call", data);
-      });
+			socket.on("my-status-for-call", data => {
+				socket.emit("receiver-status-for-call", data);
+			});
 
-      socket.on("callUser", (data) => {
-        // for (const u in user) {
-        //   if (user[u] === data.userToCall) {
-        io.emit("callUser", data);
-        //   }
-        // }
-      });
+			socket.on("callUser", data => {
+				// for (const u in user) {
+				//   if (user[u] === data.userToCall) {
+				io.emit("callUser", data);
+				//   }
+				// }
+			});
 
-      socket.on("user status to receive this call", (data) => {
-        socket.broadcast.emit("call busy", data);
-      });
+			socket.on("user status to receive this call", data => {
+				socket.broadcast.emit("call busy", data);
+			});
 
-      socket.on("receiver call you 1st", (to) => {
-        socket.broadcast.emit("receiver call you first", to);
-      });
+			socket.on("receiver call you 1st", to => {
+				socket.broadcast.emit("receiver call you first", to);
+			});
 
-      socket.on("answerCall", (data) => {
-        io.emit("callAccepted", data);
-      });
+			socket.on("answerCall", data => {
+				io.emit("callAccepted", data);
+			});
 
-      socket.on("call-reach-to-me", (to) => {
-        io.emit("call-reach-to-user", to);
-      });
+			socket.on("call-reach-to-me", to => {
+				io.emit("call-reach-to-user", to);
+			});
 
-      socket.on("cutCall", (data) => {
-        io.emit("callEnded", data.to);
-      });
+			socket.on("cutCall", data => {
+				io.emit("callEnded", data.to);
+			});
 
-      // group call
-      socket.on(
-        "members to call",
-        ({ members, callerID, callerName, roomID, callType }) => {
-          members.forEach((member) => {
-            socket.broadcast.emit("group call for you", {
-              callerID,
-              callerName,
-              member,
-              roomID,
-              callType,
-            });
-          });
-        }
-      );
-      socket.on("join room", ({ roomID, userID, userName }) => {
-        if (users[roomID?.roomId]) {
-          users[roomID?.roomId].push({ id: userID, name: userName });
-        } else {
-          users[roomID?.roomId] = [{ id: userID, name: userName }];
-        }
-        socketToRoom[userID] = roomID;
-        const usersInThisRoom = users[roomID?.roomId].filter(
-          ({ id }) => id !== userID
-        );
-        socket.emit("all users", { usersInThisRoom, roomID });
-        socket.emit("total user", { usersInThisRoom, roomID });
-      });
+			// group call
+			socket.on(
+				"members to call",
+				({ members, callerID, callerName, roomID, callType }) => {
+					members.forEach(member => {
+						socket.broadcast.emit("group call for you", {
+							callerID,
+							callerName,
+							member,
+							roomID,
+							callType,
+						});
+					});
+				}
+			);
+			socket.on("join room", ({ roomID, userID, userName }) => {
+				if (users[roomID?.roomId]) {
+					users[roomID?.roomId].push({ id: userID, name: userName });
+				} else {
+					users[roomID?.roomId] = [{ id: userID, name: userName }];
+				}
+				socketToRoom[userID] = roomID;
+				const usersInThisRoom = users[roomID?.roomId].filter(
+					({ id }) => id !== userID
+				);
+				socket.emit("all users", { usersInThisRoom, roomID });
+				socket.emit("total user", { usersInThisRoom, roomID });
+			});
 
-      socket.on(
-        "sending signal",
-        ({ roomID, userToSignal, callerID, callerName, signal }) => {
-          io.emit("user joined", {
-            roomID,
-            callerID,
-            callerName,
-            userToSignal,
-            signal,
-          });
-        }
-      );
+			socket.on(
+				"sending signal",
+				({ roomID, userToSignal, callerID, callerName, signal }) => {
+					io.emit("user joined", {
+						roomID,
+						callerID,
+						callerName,
+						userToSignal,
+						signal,
+					});
+				}
+			);
 
-      socket.on("returning signal", (payload) => {
-        io.emit("receiving returned signal", {
-          signal: payload.signal,
-          id: payload.userID,
-          roomID: payload.roomID,
-          callerID: payload.callerID,
-        });
-      });
+			socket.on("returning signal", payload => {
+				io.emit("receiving returned signal", {
+					signal: payload.signal,
+					id: payload.userID,
+					roomID: payload.roomID,
+					callerID: payload.callerID,
+				});
+			});
 
-      socket.on("cut call", (userID) => {
-        const roomID = socketToRoom[userID];
-        let room = users[roomID];
-        if (room) {
-          room = room.filter(({ id }) => id !== userID);
-          if (room.length === 0) {
-            return socket.emit("group call is closed", roomID);
-          }
-          users[roomID] = room;
-          socket.broadcast.emit("user left", userID);
-          if (room.length === 0) {
-            const usersInThisRoom = room;
-            socket.emit("total user", { usersInThisRoom, roomID });
-          }
-        }
-      });
+			socket.on("cut call", userID => {
+				const roomID = socketToRoom[userID];
+				let room = users[roomID];
+				if (room) {
+					room = room.filter(({ id }) => id !== userID);
+					if (room.length === 0) {
+						return socket.emit("group call is closed", roomID);
+					}
+					users[roomID] = room;
+					socket.broadcast.emit("user left", userID);
+					if (room.length === 0) {
+						const usersInThisRoom = room;
+						socket.emit("total user", { usersInThisRoom, roomID });
+					}
+				}
+			});
 
-      // get group call info after reloading
-      // socket.on("after reload get user data of group call", (roomID) => {
-      //   if (users[roomID].length) {
-      //     const usersInThisRoom = users[roomID];
-      //     socket.emit("total user", { usersInThisRoom, roomID });
-      //   }
-      // });
+			// get group call info after reloading
+			// socket.on("after reload get user data of group call", (roomID) => {
+			//   if (users[roomID].length) {
+			//     const usersInThisRoom = users[roomID];
+			//     socket.emit("total user", { usersInThisRoom, roomID });
+			//   }
+			// });
 
-      // disconnect
-      socket.on("disconnect", () => {
-        // private call
-        // io.emit("callEnded", data.to);
+			// disconnect
+			socket.on("disconnect", () => {
+				// private call
+				// io.emit("callEnded", data.to);
 
-        // group call
-        const roomID = socketToRoom[user[socket.id]];
-        let room = users[roomID];
-        if (room) {
-          room = room.filter(({ id }) => id !== user[socket.id]);
-          users[roomID] = room;
-          socket.broadcast.emit("user left", user[socket.id]);
-          if (room.length === 0) {
-            const usersInThisRoom = room;
-            socket.emit("total user", { usersInThisRoom, roomID });
-          }
-        }
+				// group call
+				const roomID = socketToRoom[user[socket.id]];
+				let room = users[roomID];
+				if (room) {
+					room = room.filter(({ id }) => id !== user[socket.id]);
+					users[roomID] = room;
+					socket.broadcast.emit("user left", user[socket.id]);
+					if (room.length === 0) {
+						const usersInThisRoom = room;
+						socket.emit("total user", { usersInThisRoom, roomID });
+					}
+				}
 
-        // update user status
-        userIsOffLine(user[socket.id]);
-        io.emit("user-status", { email: user[socket.id], status: "inactive" });
-        delete user[socket.id];
-      });
-    });
-  })
-  .catch((err) => console.log(err));
+				// update user status
+				userIsOffLine(user[socket.id]);
+				io.emit("user-status", { email: user[socket.id], status: "inactive" });
+				delete user[socket.id];
+			});
+		});
+	})
+	.catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-  res.status(200).send(`<h1>Node.js</h1>`);
+	res.status(200).send(`<h1>Node.js</h1>`);
 });
 
 // error handler
 const errorHandler = (err, req, res, next) => {
-  if (res.headersSent) {
-    return next(err);
-  }
-  res.status(400).send(err);
+	if (res.headersSent) {
+		return next(err);
+	}
+	res.status(400).send(err);
 };
 
 app.use("/user/account", userAccount);
@@ -215,5 +218,5 @@ app.use("/jwt", generateNewToken);
 app.use(errorHandler);
 
 httpServer.listen(process.env.PORT || 5000, () =>
-  console.log("Server is running")
+	console.log("Server is running")
 );
--- route/message.js
+++ route/message.js
@@ -10,246 +10,257 @@ dotenv.config();
 const router = express.Router();
 
 const OneOneChat = mongoose.model(
-  `${process.env.ONE_ONE_CHAT_COLLECTION}`,
-  one_one_schema
+	`${process.env.ONE_ONE_CHAT_COLLECTION}`,
+	one_one_schema
 );
 
 export const oneOneMessageFromSocket = (socket, roomId) => {
-  const newMessage = mongoose.connection.collection("one_one_messages").watch();
-  newMessage.on("change", (change) => {
-    // console.log(change);
-    if (change.operationType === "insert") {
-      const message = change.fullDocument;
-      socket.emit("new-message", message);
-    } else if (change.operationType === "update") {
-      const updateFiled = change?.updateDescription?.updatedFields;
-      if (updateFiled?.status) {
-        return socket.emit("updated-message-status", {
-          _id: change?.documentKey?._id,
-          status: updateFiled?.status,
-        });
-      }
-      socket.emit("update-react", {
-        _id: change?.documentKey?._id,
-        react:
-          updateFiled?.react?.length >= 0 ? updateFiled.react : updateFiled,
-      });
-    } else if (change.operationType === "delete") {
-      socket.emit("delete-chatMessage", { _id: change?.documentKey?._id });
-    }
-  });
-  socket.on("typing", (info) => {
-    socket.broadcast.emit("displayTyping", info);
-  });
+	const newMessage = mongoose.connection.collection("one_one_messages").watch();
+	newMessage.on("change", change => {
+		// console.log(change);
+		if (change.operationType === "insert") {
+			const message = change.fullDocument;
+			socket.emit("new-message", message);
+		} else if (change.operationType === "update") {
+			const updateFiled = change?.updateDescription?.updatedFields;
+			if (updateFiled?.status) {
+				return socket.emit("updated-message-status", {
+					_id: change?.documentKey?._id,
+					status: updateFiled?.status,
+				});
+			}
+			socket.emit("update-react", {
+				_id: change?.documentKey?._id,
+				react:
+					updateFiled?.react?.length >= 0 ? updateFiled.react : updateFiled,
+			});
+		} else if (change.operationType === "delete") {
+			socket.emit("delete-chatMessage", { _id: change?.documentKey?._id });
+		}
+	});
+	socket.on("typing", info => {
+		socket.broadcast.emit("displayTyping", info);
+	});
 };
 
 let gfs;
 mongoose.connection.once("open", () => {
-  gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
-    bucketName: `${process.env.ONE_ONE_CHAT_COLLECTION}`,
-  });
+	gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+		bucketName: `${process.env.ONE_ONE_CHAT_COLLECTION}`,
+	});
 });
+
+// const logger = (req, res, next) => {
+// 	console.log(req.body, "and", req.files);
+// 	next();
+// };
 
 router.post("/upload", checkLogin, uploadMiddleware, (req, res) => {
-  let files = [];
-  for (let i = 0; i < req.files.length; i++) {
-    const element = req.files[i];
-    files[i] = {
-      fileId: element.id,
-      filename: element.filename,
-      contentType: element.contentType,
-    };
-  }
+	let files = [];
+	console.log(req.files);
+	// Bad request: handle route if empty file
+	if (req.files.length === 0) {
+		return res.status(400).send("File not found");
+	}
 
-  const fileInfo = {
-    id: req.body.id,
-    sender: req.body.sender,
-    files,
-    react: [],
-    status: req.body.status,
-    timeStamp: req.body.timeStamp,
-  };
+	for (let i = 0; i < req.files.length; i++) {
+		const element = req.files[i];
+		files[i] = {
+			fileId: element.id,
+			filename: element.filename,
+			contentType: element.contentType,
+		};
+	}
 
-  const newFiles = new OneOneChat(fileInfo);
-  newFiles.save((err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    } else {
-      return res.status(200).send(result);
-    }
-  });
+	const fileInfo = {
+		id: req.body.id,
+		sender: req.body.sender,
+		files,
+		react: [],
+		status: req.body.status,
+		timeStamp: req.body.timeStamp,
+	};
+
+	const newFiles = new OneOneChat(fileInfo);
+	newFiles.save((err, result) => {
+		if (err) {
+			return res.status(500).send(err);
+		} else {
+			return res.status(200).send(result);
+		}
+	});
 });
 router.get("/file/:filename", (req, res) => {
-  gfs.find({ filename: req.params.filename }).toArray((err, files) => {
-    if (err) {
-      return res.status(404).send(err.message);
-    } else {
-      const contentType = files[0].contentType;
-      //setting response header
-      res.set({
-        "Accept-Ranges": "bytes",
-        // "Content-Disposition": `attachment; filename=${req.params.filename}`,
-        "Content-Type": `${contentType}`,
-        "Access-Control-Allow-Origin": "*",
-      });
+	gfs.find({ filename: req.params.filename }).toArray((err, files) => {
+		if (err) {
+			return res.status(404).send(err.message);
+		} else {
+			const contentType = files[0].contentType;
+			//setting response header
+			res.set({
+				"Accept-Ranges": "bytes",
+				// "Content-Disposition": `attachment; filename=${req.params.filename}`,
+				"Content-Type": `${contentType}`,
+				"Access-Control-Allow-Origin": "*",
+			});
 
-      if (!files || files.length === 0)
-        return res.status(400).send("file not exist");
+			if (!files || files.length === 0)
+				return res.status(400).send("file not exist");
 
-      const downloadStream = gfs.openDownloadStreamByName(req.params.filename);
-      downloadStream.on("error", (err) => {
-        return res.status(404).send(err.message);
-      });
-      return downloadStream.pipe(res);
-    }
-  });
+			const downloadStream = gfs.openDownloadStreamByName(req.params.filename);
+			downloadStream.on("error", err => {
+				return res.status(404).send(err.message);
+			});
+			return downloadStream.pipe(res);
+		}
+	});
 });
 
 router.delete("/file/delete/:id", checkLogin, (req, res) => {
-  const _id = new mongoose.Types.ObjectId(req.params.id);
-  gfs.delete(_id, (err, gridStore) => {
-    if (err) {
-      return res.status(404).send(err.message);
-    } else {
-      return res.status(200).send("deleted successfully");
-    }
-  });
+	const _id = new mongoose.Types.ObjectId(req.params.id);
+	gfs.delete(_id, (err, gridStore) => {
+		if (err) {
+			return res.status(404).send(err.message);
+		} else {
+			return res.status(200).send("deleted successfully");
+		}
+	});
 });
 
 router.post("/postOneOneChat", checkLogin, (req, res) => {
-  const chatMessage = new OneOneChat(req.body);
-  chatMessage.save((err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    } else {
-      return res.status(200).send(result.insertCount > 0);
-    }
-  });
+	const chatMessage = new OneOneChat(req.body);
+	chatMessage.save((err, result) => {
+		if (err) {
+			return res.status(500).send(err);
+		} else {
+			return res.status(200).send(result.insertCount > 0);
+		}
+	});
 });
 
 router.post("/postCallInfo", checkLogin, (req, res) => {
-  const callInfo = new OneOneChat(req.body);
-  callInfo.save((err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    } else {
-      return res.status(201).send(result);
-    }
-  });
+	const callInfo = new OneOneChat(req.body);
+	callInfo.save((err, result) => {
+		if (err) {
+			return res.status(500).send(err);
+		} else {
+			return res.status(201).send(result);
+		}
+	});
 });
 
 router.post("/getOneOneChat/:roomId", checkLogin, (req, res) => {
-  const itemsPerPage = 35;
-  const pageNum = parseInt(req.body.pageNum, 10);
-  OneOneChat.find({ id: req.params.roomId })
-    .sort({ _id: -1 })
-    .skip(itemsPerPage * (pageNum - 1))
-    .limit(itemsPerPage)
-    .then((docs) => {
-      return res.status(200).send(docs);
-    })
-    .catch((err) => {
-      return res.status(404).send(err);
-    });
+	const itemsPerPage = 35;
+	const pageNum = parseInt(req.body.pageNum, 10);
+	OneOneChat.find({ id: req.params.roomId })
+		.sort({ _id: -1 })
+		.skip(itemsPerPage * (pageNum - 1))
+		.limit(itemsPerPage)
+		.then(docs => {
+			return res.status(200).send(docs);
+		})
+		.catch(err => {
+			return res.status(404).send(err);
+		});
 });
 
 router.put("/updateChatMessage", checkLogin, (req, res) => {
-  OneOneChat.findOneAndUpdate(
-    { _id: req.body.id },
-    {
-      $addToSet: { react: req.body.reactInfo },
-    },
-    (err, result) => {
-      if (err) {
-        return res.status(404).send(err);
-      } else {
-        return res.status(200).send(result);
-      }
-    }
-  );
+	OneOneChat.findOneAndUpdate(
+		{ _id: req.body.id },
+		{
+			$addToSet: { react: req.body.reactInfo },
+		},
+		(err, result) => {
+			if (err) {
+				return res.status(404).send(err);
+			} else {
+				return res.status(200).send(result);
+			}
+		}
+	);
 });
 
 router.put("/updateOnlyReact", checkLogin, (req, res) => {
-  OneOneChat.findOneAndUpdate(
-    { _id: req.body.id, "react.sender": req.body.sender },
-    {
-      $set: { "react.$.react": req.body.react },
-    },
-    (err, result) => {
-      if (err) {
-        return res.status(404).send(err);
-      } else {
-        return res.status(200).send(result);
-      }
-    }
-  );
+	OneOneChat.findOneAndUpdate(
+		{ _id: req.body.id, "react.sender": req.body.sender },
+		{
+			$set: { "react.$.react": req.body.react },
+		},
+		(err, result) => {
+			if (err) {
+				return res.status(404).send(err);
+			} else {
+				return res.status(200).send(result);
+			}
+		}
+	);
 });
 
 router.put("/removeReact", checkLogin, (req, res) => {
-  OneOneChat.findOneAndUpdate(
-    { _id: req.body.id },
-    {
-      $pull: { react: { sender: req.body.sender } },
-    },
-    (err, result) => {
-      if (err) {
-        return res.status(404).send(err);
-      } else {
-        return res.status(200).send(result);
-      }
-    }
-  );
+	OneOneChat.findOneAndUpdate(
+		{ _id: req.body.id },
+		{
+			$pull: { react: { sender: req.body.sender } },
+		},
+		(err, result) => {
+			if (err) {
+				return res.status(404).send(err);
+			} else {
+				return res.status(200).send(result);
+			}
+		}
+	);
 });
 
 router.delete("/deleteChatMessage/:id", checkLogin, (req, res) => {
-  OneOneChat.deleteOne({ _id: req.params.id }, (err, result) => {
-    if (err) {
-      return res.status(404).send(err);
-    } else {
-      return res.status(200).send(result);
-    }
-  });
+	OneOneChat.deleteOne({ _id: req.params.id }, (err, result) => {
+		if (err) {
+			return res.status(404).send(err);
+		} else {
+			return res.status(200).send(result);
+		}
+	});
 });
 
 router.post("/unseen-message-to-seen", checkLogin, (req, res) => {
-  const ids = req.body;
-  let count = 0;
+	const ids = req.body;
+	let count = 0;
 
-  ids.forEach((id) => {
-    const _id = new mongoose.Types.ObjectId(id);
-    OneOneChat.updateOne(
-      { _id },
-      {
-        $set: { status: "seen" },
-      },
-      (err) => {
-        if (err) return res.status(500).send(err.message);
-        count += 1;
-        if (ids.length === count) {
-          return res.status(200).send("update successfully.");
-        }
-      }
-    );
-  });
+	ids.forEach(id => {
+		const _id = new mongoose.Types.ObjectId(id);
+		OneOneChat.updateOne(
+			{ _id },
+			{
+				$set: { status: "seen" },
+			},
+			err => {
+				if (err) return res.status(500).send(err.message);
+				count += 1;
+				if (ids.length === count) {
+					return res.status(200).send("update successfully.");
+				}
+			}
+		);
+	});
 });
 
 router.get("/get-lastMessage-for-chatBar/:id", checkLogin, (req, res) => {
-  OneOneChat.findOne({ id: req.params.id })
-    .sort({ _id: -1 })
-    .then((message) => {
-      res.status(200).send(message);
-    })
-    .catch((err) => {
-      res.status(404).send(err.message);
-    });
+	OneOneChat.findOne({ id: req.params.id })
+		.sort({ _id: -1 })
+		.then(message => {
+			res.status(200).send(message);
+		})
+		.catch(err => {
+			res.status(404).send(err.message);
+		});
 });
 
 router.delete("/delete-conversation/:id", checkLogin, (req, res) => {
-  const id = req.params.id;
-  OneOneChat.deleteMany({ id }, (err) => {
-    if (err) return res.status(501).send(err.message);
-    return res.status(200).send("deleted successfully");
-  });
+	const id = req.params.id;
+	OneOneChat.deleteMany({ id }, err => {
+		if (err) return res.status(501).send(err.message);
+		return res.status(200).send("deleted successfully");
+	});
 });
 
 export default router;
